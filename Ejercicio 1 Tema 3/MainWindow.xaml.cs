d61f70eb8f1c7b8c38bd336ac74869b99c309719
Maria <Maria@DESKTOP-G07TP2O>
2020-11-09 18:15:22 +0100

    Ejercicio 1 Tema 3 añadido

---
+++ Ejercicio 1 Tema 3/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Ejercicio_1_Tema_3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
